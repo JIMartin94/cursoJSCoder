dcafe0584d0271bd7dd958011d8c317abc58b6af
juaniMartin <juani-martin@hotmail.com>
2022-05-23 23:39:37 -0300

    agregando fetch

--- js/carrito.js
+++ js/carrito.js
@@ -1,13 +1,10 @@
-
 // RECIBO PRODUCTOS PARA EL CARRITO
 arrayProductosCarrito = JSON.parse(localStorage.getItem("productosCarrito"));
 
-let div_carritoCompras = document.getElementById("carritoCompras");
-
-
+const div_carritoCompras = document.getElementById("carritoCompras");
 
 // FUNCION PARA MOSTRAR CARRITO
-const mostrarCarrito = () =>{
+const mostrarCarrito = (arrayProductosCarrito) =>{
 
     div_carritoCompras.innerHTML= " ";
 
@@ -17,95 +14,40 @@ const mostrarCarrito = () =>{
     for(item of arrayProductosCarrito){
 
         let div_card = document.createElement("div");
-        div_card.className = "card card-body col-lg-12";
-        div_card.id = "general";
-
-        let div_gen = document.createElement("div");
-        div_gen.className = "media align-items-center align-items-lg-start text-lg-left flex-column flex-lg-row";
-
-        let div_img = document.createElement("div");
-        div_img.className = "mr-2 mb-3 mb-lg-0";
-
-        let img = document.createElement("img");
-        img.src = "../"+item.foto;
-        img.width = "150";
-        img.height = "100";
-
-        let div_media_body = document.createElement("div");
-        div_media_body.className = "media-body";
-
-        let h6 = document.createElement("h6");
-        h6.className = "media-title font-weight-semibold";
-
-        let a = document.createElement("a");
-        a.href = "#";
-        a.innerHTML = item.nombre;
-
-        let label = document.createElement("label");
-        label.innerHTML = "Cantidad";
-        label.className = "mr-2"
-
-        let select = document.createElement("select");
-        select.id = item.id;
-        select.className = "cantidad";
-
-        let op = document.createElement("option");
-        op.innerHTML = item.cantidad;
-        op.value = "1";
-
-        let op1 = document.createElement("option");
-        op1.innerHTML = "1";
-        op1.value = "1";
-
-        let op2 = document.createElement("option");
-        op2.innerHTML = "2";
-        op2.value = "2";
-
-        let op3= document.createElement("option");
-        op3.innerHTML = "3";
-        op3.value = "3";
-
-        let op4 = document.createElement("option");
-        op4.innerHTML = "4";
-        op4.value = "4";
-
-        let op5 = document.createElement("option");
-        op5.innerHTML = "5";
-        op5.value = "5";
-
-        let div_precio = document.createElement("div");
-        div_precio.className = "mt-3 mt-lg-0 ml-lg-3 text-right pr-5 ";
-
-        let h3 = document.createElement("h3");
-        h3.id = "precio" + item.id;
-        h3.className = "mb-0 font-weight-semibold";
-        h3.innerHTML = "$" + (item.precio * item.cantidad);
-
-        let div_estrella = document.createElement("div");
-
-        let i1 = document.createElement("i");
-        i1.className = "fa fa-star";
-
-        let i2 = document.createElement("i");
-        i2.className = "fa fa-star";
-
-        let i3 = document.createElement("i");
-        i3.className = "fa fa-star";
-
-        let i4 = document.createElement("i");
-        i4.className = "fa fa-star";
-
-        let i5 = document.createElement("i");
-        i5.className = "fa fa-star";
-
-        div_img.append(img);
-        h6.append(a);
-        select.append(op,op1,op2,op3,op4,op5);
-        div_estrella.append(i1,i2,i3,i4,i5);
-        div_precio.append(h3,div_estrella);
-        div_media_body.append(h6,label,select,div_precio);
-        div_gen.append(div_img,div_media_body);
-        div_card.append(div_gen);
+        div_card.innerHTML = '<div id="general "class="card card-body col-lg-12">'+
+                                '<div class="media align-items-center align-items-lg-start text-lg-left flex-column flex-lg-row">'+
+                                    '<div class="mr-2 mb-3 mb-lg-0">'+
+                                        '<img src="../'+ item.imagen +'" width="150" height="100">'+
+                                    '</div>'+
+                                    '<div class="media-body">'+    
+                                        '<h6 class="media-title font-weight-semibold">'+
+                                            '<a href="#">'+item.nombre+'</a>'+
+                                            '<a onclick="eliminarProd('+item.id+')" id="eliminar'+item.id+'" class="fa-solid fa-circle-xmark eliminar"></a>'+
+                                        '</h6>'+
+                                        '<label class="mr-2">Cantidad</label>'+
+                                        '<select class="cantidad" id="'+ item.id +'">'+
+                                            '<option>'+ item.cantidad +'</option>'+
+                                            '<option value="1">1</option>'+
+                                            '<option value="2">2</option>'+
+                                            '<option value="3">3</option>'+
+                                            '<option value="4">4</option>'+
+                                            '<option value="5">5</option>'+
+                                        '</select>'+
+                                        '<div class="mt-3 mt-lg-0 ml-lg-3 text-right pr-5 ">'+
+                                            '<h3 id="precio'+ item.id+'" class="mb-0 font-weight-semibold">$ '+ (item.precio * item.cantidad) +'</h3>'+
+                                        '<div>'+
+                                            '<i class="fa fa-star"></i>'+
+                                            '<i class="fa fa-star"></i>'+
+                                            '<i class="fa fa-star"></i>'+
+                                            '<i class="fa fa-star"></i>'+
+                                            '<i class="fa fa-star"></i>'+
+                                        '</div>'+
+                                        '<div class="text-muted">1985 reviews</div>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>';
+        
         div_carritoCompras.append(div_card);
 
         totalCompra += (item.precio * item.cantidad);
@@ -118,7 +60,30 @@ const mostrarCarrito = () =>{
 }
 
 
-mostrarCarrito();
+mostrarCarrito(arrayProductosCarrito);
+
+const eliminarProd = (id) =>{
+
+    let indice = arrayProductosCarrito.findIndex((elemento)=>{
+        return elemento.id == id;
+    });
+
+    arrayProductosCarrito.splice(indice,1);
+
+    if(arrayProductosCarrito.length == 0){
+        Swal.fire({
+            title: "El carrito esta vacio!",
+            icon: "error",
+            timer: 3000  
+        }).then( () =>{
+            localStorage.clear();
+            window.location.href = '../index.html';
+            });    
+    }else{
+        localStorage.setItem("productosCarrito", JSON.stringify(arrayProductosCarrito));
+        mostrarCarrito(arrayProductosCarrito);
+    } 
+}
 
 // FUNCION PARA CAMBIAR LA CANTIDAD DE PRODUCTOS
 const cambiarCantProd = (id,lista,cantidad) =>{
@@ -136,22 +101,16 @@ const clickearCant = e => {
    }
 }
 
-
-let precio = document.getElementById("precio");
-let select = document.getElementById("cantidad");
-let carritoCompras = document.getElementById("carritoCompras");
-
-
 //ELIJO LA CANTIDAD DE CADA PRODUCTO
-carritoCompras.addEventListener("input", e => {
+div_carritoCompras.addEventListener("input", e => {
     let producto = clickearCant(e);
     cambiarCantProd(producto.id,arrayProductosCarrito,producto.value);
-    mostrarCarrito();
+    localStorage.setItem("productosCarrito", JSON.stringify(arrayProductosCarrito));
+    mostrarCarrito(arrayProductosCarrito);
 });
 
-
+// FINALIZAR LA COMPRA
 let botonFinalizar = document.getElementById("botonFinalizar");
-
 botonFinalizar.addEventListener("click", () => {
 
     Swal.fire({
@@ -161,46 +120,8 @@ botonFinalizar.addEventListener("click", () => {
         confirmButtonText: "Terminar",
 
     }).then( () =>{
+        localStorage.clear();
         window.location.href = '../index.html';
      });
     
 });
-
-
-/*
-<div class="card card-body col-lg-12">
-                    <div class="media align-items-center align-items-lg-start text-lg-left flex-column flex-lg-row">
-                        <div class="mr-2 mb-3 mb-lg-0">
-                            <img src="../assets/img/auriculares.jpg" width="150" height="150" alt="">
-                        </div>
-
-                        <div class="media-body">
-                            <h6 class="media-title font-weight-semibold">
-                                <a href="#">Apple iPhone XR (Red, 128 GB)</a>
-                            </h6>
-
-                            <label for="">Cantidad</label>
-                            <select name="" id="">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-
-                            <div class="mt-3 mt-lg-0 ml-lg-3 text-right pr-5 ">
-                                <h3 class="mb-0 font-weight-semibold">$459.99</h3>
-
-                                <div>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                </div>
-
-                                <div class="text-muted">1985 reviews</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>*/
--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
-
-let arrayProductos = [];
+const arrayProductos = [];
 let arrayCarrito = [];
-
-class Producto{
-    
-    constructor(id,nombre,cantidad,foto,precio){
-        this.id = id;
-        this.nombre = nombre;
-        this.cantidad = cantidad;
-        this.foto = foto,
-        this.precio = precio;
-    }
-}
-
-// INGRESO LOS PRODUCTOS QUE ESTAN A LA VENTA
-const mouseLogi = new Producto(1,"Logitech M170/4940 Mouse Optico - Inalambrico Negro",1,"assets/img/mouse.jpg",750);
-const microfonoAsus = new Producto(2,"MICROFONO HYPERX QUADCAST S PC/PS4",1,"assets/img/microfono.jpg",5000);
-const auriculares = new Producto(3,"Auriculares gamer para PC PS4 ANDROID 7.1 USB EXTRA BASS",1,"assets/img/auriculares.jpg",3000);
-const monitor = new Producto(4,"Monitor 24MK430H-B-B de 60,4 cm (23,8 pulgadas) 1920 x 1080",1,"assets/img/monitor.jpg",20000);
-const routerGaming = new Producto(5,"Router gaming ASUS Wifi 6",1,"assets/img/router_gaming.jpg",2000);
-const gabinete = new Producto(6,"Gabinete gamer raidmax X616TBF",1,"assets/img/gabinete.jpg",5000);
-const tabletSamsung = new Producto(7,"Tablet Samsung Galaxy Tab S7",1,"assets/img/tablet_Samsung.jpg",35000);
-const notebookAsus = new Producto(8,"Notebook ASUS I3-1005GI 4GB 1TB 15.6 1920X1080 X509JA-EJ062T",1,"assets/img/notebook_asus.jpg",150000);
-
-arrayProductos.push(mouseLogi,microfonoAsus,auriculares,monitor,routerGaming,gabinete,tabletSamsung,notebookAsus);
 
 // FUNCION PARA MOSTRAR TODOS LOS PRODUCTOS
 const mostrarProductos = (arrayProductos) =>{
@@ -32,47 +8,47 @@ const mostrarProductos = (arrayProductos) =>{
     div_productos.innerHTML = " ";
 
     for(item of arrayProductos){
+        let div_col = document.createElement("div");
+        div_col.className = "col-lg-3";
+        div_col.innerHTML = '<div class="productos_item">'+
+                                '<img src="' + item.imagen +'" alt="producto" class ="productos_item_imagen">'+
+                                '<div>'+
+                                    '<a href="#" class ="productos_item_texto">'+ item.nombre+'</a>'+
+                                    '<div>'+
+                                        '<p> $'+ item.precio +'</p>'+
+                                        '<button type="button" class="btn btn-primary agregar" id="'+ item.id +'" >Agregar al carrito</button>'+
+                                    '</div>'+
+                                '</div>'+
+                            '</div>';
+        div_productos.append(div_col);
+    }
 
+    if(localStorage.length > 0){
+        arrayCarrito = JSON.parse(localStorage.getItem("productosCarrito"));
+        carrito_cant.innerHTML = arrayCarrito.length
 
-    let div_col = document.createElement("div");
-    div_col.className = "col-lg-3";
-
-    let div_item = document.createElement("div");
-    div_item.className = "productos_item";
-
-
-    let img = document.createElement("img");
-    img.className = "productos_item_imagen";
-    img.alt = "producto";
-    img.src = item.foto;
-
-    let div_info = document.createElement("div");
-
-    let a = document.createElement("a");
-    a.className = "productos_item_texto";
-    a.href = "#";
-    a.innerHTML = item.nombre;
-
-    let div_precio = document.createElement("div");
-
-    let precio = document.createElement("p");
-    precio.innerHTML = "$ " + item.precio;
-
-    let boton_agregar = document.createElement("button");
-    boton_agregar.type = "button";
-    boton_agregar.className = "btn btn-primary agregar";
-    boton_agregar.innerHTML = "Agregar al carrito";
-    boton_agregar.id = item.id;
-
-    div_precio.append(precio,boton_agregar);
-    div_info.append(a,div_precio);
-    div_item.append(img,div_info);
-    div_col.append(div_item);
-    div_productos.append(div_col);
+        let cantidad = 0;
+          for(let item of arrayCarrito){
+              cantidad +=  parseInt(item.cantidad);
+          }
+          carrito_cant.innerHTML =  cantidad;
     }
 }
 
-mostrarProductos(arrayProductos);
+const obtenerProductos = async () =>{
+
+    const responseProductos = await fetch("../jsons/productos.json");
+
+    const productos = await responseProductos.json();
+
+    for(let producto of productos){
+        arrayProductos.push(producto);
+    }
+    mostrarProductos(arrayProductos);
+}
+
+obtenerProductos();
+
 
 const encontrarPalabra = (palabra,frase) =>{
 
@@ -86,9 +62,8 @@ buscarp.addEventListener("input", () =>{
     let arrayP = [];
     for(let prod of arrayProductos){
         if(encontrarPalabra((buscarp.value).toUpperCase(),(prod.nombre).toUpperCase())){
-            console.log("encontro");
-             arrayP.push(prod);        
-        }       
+             arrayP.push(prod);
+        }
     }
     return arrayP.length == 0 ? mostrarProductos(arrayProductos) : mostrarProductos(arrayP);
 });
@@ -102,17 +77,17 @@ const clickearId = e => {
 
 
 //BUSCO EL ELEMENTO DE UNA ARRAY POR EL ID
-const buscarProd = (id,lista) =>{ 
-    return lista.find((elemento)=>{ 
+const buscarProd = (id,lista) =>{
+    return lista.find((elemento)=>{
         if(elemento.id == id){
             return elemento;
-        } 
+        }
     });
 }
 
 //GUARDO CADA PRODUCTO EN EL ARRAY
 let row_productos = document.querySelector(".row_productos");
-let carrito_cant = document.getElementById("carrito_cant");
+const carrito_cant = document.getElementById("carrito_cant");
 
 row_productos.addEventListener("click", e => {
     let id = clickearId(e);
@@ -127,19 +102,23 @@ row_productos.addEventListener("click", e => {
     }else{
         arrayCarrito.push(buscarProd(id,arrayProductos));
         Toastify({
-            text: "Agregado al carrito", 
+            text: "Agregado al carrito",
             className: "info",
-            duration: 3000,  
+            duration: 3000,
             style: {
               background: "linear-gradient(to right, #00b09b, #96c93d)",
             }
           }).showToast();
-          carrito_cant.innerHTML =  arrayCarrito.length;
+          let cantidad = 0;
+          for(let item of arrayCarrito){
+              cantidad +=  parseInt(item.cantidad);
+          }
+          carrito_cant.innerHTML =  cantidad;
     }
-    
+
 });
 
-//CLICKEO EL CARRITO Y GUARDO EL ARRAY DE PRODUCTOS 
+//CLICKEO EL CARRITO Y GUARDO EL ARRAY DE PRODUCTOS
 let carrito = document.getElementById("carrito");
 carrito.addEventListener("click", () => {
 
@@ -152,19 +131,7 @@ carrito.addEventListener("click", () => {
         })
     }else{
         carrito.href="view/carrito.html";
-        localStorage.setItem("productosCarrito", JSON.stringify(arrayCarrito));  
+        localStorage.setItem("productosCarrito", JSON.stringify(arrayCarrito));
     }
-         
+
 });
-
-
-/* <div class="col-lg-3">
-    <div class="productos_item">
-        <img src="assets/img/mouse.jpg" alt="producto2" class ="productos_item_imagen">
-        <div>
-            <a href="#" class ="productos_item_texto">Logitech M170/4940 Mouse Optico - Inalambrico Negro</a>    
-            <p>$ 153.00</p>
-            <button type="button" class="btn btn-primary">Agregar al carrito</button>
-        </div>
-    </div>
-</div> */
